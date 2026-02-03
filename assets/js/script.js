e0502ce67a422131d8602e87afb09a9706336215
Daniel Inaju <88445122+Dannyecho@users.noreply.github.com>
2026-02-03 04:28:45 +0100

    Added isPrime Function

--- assets/js/script.js
+++ assets/js/script.js
@@ -285,6 +285,29 @@ function applyLogarithm() {
   updateResult();
 }
 
+function isPrime(num) {
+  // Numbers less than 2 are not prime
+  if (num <= 1) {
+    return false;
+  }
+
+  if (num === 2) {
+    return true;
+  }
+
+  if (num % 2 === 0) {
+    return false;
+  }
+
+  const limit = Math.sqrt(num);
+  for (let i = 3; i <= limit; i += 2) {
+    if (num % i === 0) {
+      return false; 
+    }
+  }
+
+  return true;
+}
 
 function checkPrime() {
     const num = parseFloat(currentExpression);
@@ -486,4 +509,5 @@ function percentToResult() {
     }
 }
   
+
 
